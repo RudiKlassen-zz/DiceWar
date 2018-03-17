da7d9ca850aba2f5050824f199423d9a77b9e080
Rudi Klassen <rudolfklassen2@gmail.com>
2018-03-17 19:31:02 +0100

    Setup Applicationsettings

---
+++ core/src/com/mopix/diewar/Config.java
@@ -0,0 +1,16 @@
+package com.mopix.diewar;
+
+import com.badlogic.gdx.Application;
+
+public class Config {
+
+    /*Application*/
+    public static final int APPLICATION_WIDTH = 1280;
+    public static final int APPLICATION_HEIGHT = 900;
+    public static final int LOG_LEVEL = Application.LOG_INFO;
+    public static final int BACKGROUND_FPS = 60;
+    public static final int FOREGROUND_FPS = 60;
+    public static final int SAMPLES = 4; /** number of samples for MSAA **/
+    public static final boolean FULLSCREEN = false;
+    public static final boolean V_SYNC_ENABLED = false;
+}
--- desktop/src/com/mopix/diewar/desktop/DesktopLauncher.java
+++ desktop/src/com/mopix/diewar/desktop/DesktopLauncher.java
@@ -1,7 +1,10 @@
 package com.mopix.diewar.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mopix.diewar.Config;
 import com.mopix.diewar.DiceWar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,19 @@ public class DesktopLauncher {
 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "DiceWar";
+		config.fullscreen = Config.FULLSCREEN;
+		config.vSyncEnabled = Config.V_SYNC_ENABLED;
+		config.backgroundFPS = Config.BACKGROUND_FPS;
+		config.foregroundFPS = Config.FOREGROUND_FPS;
+		config.samples = Config.SAMPLES;
+		config.width = Config.APPLICATION_WIDTH;
+		config.height = Config.APPLICATION_HEIGHT;
+		config.addIcon("icon.png", Files.FileType.Internal);
+
 		new LwjglApplication(new DiceWar(), config);
+		Gdx.app.setLogLevel(Config.LOG_LEVEL);
+		LOGGER.info("Desktop launch started");
 	}
 }

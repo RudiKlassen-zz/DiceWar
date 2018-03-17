fc579ed6b254634d1289e8a021a598337ddf2b2d
Rudi Klassen <rudolfklassen2@gmail.com>
2018-03-17 19:21:05 +0100

    Added slf4j Logger

--- desktop/src/com/mopix/diewar/desktop/DesktopLauncher.java
+++ desktop/src/com/mopix/diewar/desktop/DesktopLauncher.java
@@ -3,8 +3,13 @@ package com.mopix.diewar.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mopix.diewar.DiceWar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DesktopLauncher {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DesktopLauncher.class);
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new DiceWar(), config);

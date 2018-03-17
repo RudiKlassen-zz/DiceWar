5084b26fdfe92b2db974e0af1be819995965e743
Rudi Klassen <rudolfklassen2@gmail.com>
2018-03-17 19:07:56 +0100

    init libgdx dicewar project

---
+++ desktop/src/com/mopix/diewar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mopix.diewar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mopix.diewar.DiceWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiceWar(), config);
+	}
+}
